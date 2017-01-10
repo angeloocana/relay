5bee19627e1b537c76fac92d6e4ba020d02e7095
Angelo Ocana <angeloocana@gmail.com>
2017-01-10 21:29:03 +0000

    Configuring babel and webpack

---
+++ js/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Hello from "./components/Hello";
+
+console.log("Hello js 2");
+
+ReactDOM.render(<Hello />, document.getElementById('react'));
